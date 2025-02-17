0d865cfe2f1cef04be2e01ff731219aeadcf12a7
ANANTA FIRDAUS <ananta23ti@mahasiswa.pcr.ac.id>
2025-02-17 09:52:32 +0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from './components/header/header.jsx'
+import Hero from './components/hero/hero.jsx'
+import './App.css'
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Hero />
+    </div>
+  );
+  
+}
+
+export default App
