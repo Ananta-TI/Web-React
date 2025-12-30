bb81b17c6a9597eec0a8a3bfb7ab2b3db080b97b
ANANTA FIRDAUS <ananta23ti@mahasiswa.pcr.ac.id>
2025-12-31 02:59:58 +0700

    scanner sialan4

--- src/layouts/scanner.jsx
+++ src/layouts/scanner.jsx
@@ -203,7 +203,7 @@ const abortRef = React.useRef(null);
   const BACKEND_URL = (import.meta.env.VITE_BACKEND_URL) ||
     (window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1"
       ? "http://localhost:5000"
-      : "https://ananta-ti--ananta23ti.replit.app");
+      : "https://ananta-ti.vercel.app");
 
   // --- HISTORY LOGIC ---
   useEffect(() => {
