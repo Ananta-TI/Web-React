fa50b8708bb361f18692dc33bab4e09c6b2c3d74
ANANTA FIRDAUS <ananta23ti@mahasiswa.pcr.ac.id>
2025-12-31 04:19:37 +0700

    scanner monyet -2

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,9 +1,8 @@
-// supabaseClient.js (Correct for Next.js)
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
-
-const supabase = createClient(supabaseUrl, supabaseAnonKey);
+const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY
+);
 
 export default supabase;
